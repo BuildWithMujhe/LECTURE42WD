7bc854e66ab9710f39904912cddceab17edca10d
Abhinav Kumar <kumarabhinav1908@gmail.com>
2026-06-16 20:35:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,157 @@
+// console.log("Abhinav");
+
+// for(let i=1; i<=10; i++) {
+//     console.log("Abhinav")
+// }
+
+// for(let i=1; i<=5; i++) {
+//     console.log(i);
+// }
+
+// for(let i=5; i>0; i--) {
+//     console.log(i);
+// }
+
+// for(let i=1; i<=6; i++) {
+//     if(i==4) {
+//         break;
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 1; i<=4; i++) {
+//     if(i == 3)
+//         continue;
+//     else 
+//         console.log(i);
+// }
+
+// let i = 1;
+// while(i <= 10) {
+//     console.log("Abhinav");
+//     i++;
+// }
+
+// let i = 1;
+// while(i<6) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 5;
+// while(i>0) {
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 1;
+// while(i<5)
+// {
+//     if (i==3)
+//         break;
+//     else
+//         console.log(i)
+//     i++;
+// }
+
+
+// let i = 1;
+// while(i<5)
+// {
+//     if(i==3)
+//         continue;
+//     else 
+//         console.log("Hi")
+//     i++;
+// }
+
+// let i = 1;
+// while(i < 5) {
+//     console.log("Inside the loop");
+//     if(i==3){
+//         i++;
+//         continue;
+//     }
+//     else
+//         console.log(i);
+//     i++;
+// }
+
+// let i = 1;
+// do{
+//     console.log("Abhinav");
+//  Updation
+//     i++;
+// }while(i<=10);
+
+
+// let i = 1;
+// do{
+//     console.log(i);
+//  Updation
+//     i++;
+// }while(i<6);
+
+
+// let i = 5;
+// do{
+//     console.log(i);
+//     Updation
+//     i--;
+// } while(i>0);
+
+
+// let firstName = "Abhinav";
+// let lastName = 'Kumar';
+
+// let name = `Abhinav
+// Kumar This is
+// my
+// name
+// hello`;
+
+// console.log(name);
+
+
+// let firstName = new String("Abhinav Kumar");
+// console.log(firstName);
+
+// let op1 = 'English';
+// let op2 = 'HINDI';
+
+// console.log(op2.length);
+
+// let finalAns = `${op1} + ${op2}`;
+// let finalAns = `${op1} and ${op2}`;
+// let finalAns = `${op1}${op2}`;
+// console.log(finalAns)
+
+// let finalAns = `op1 + op2`;
+// console.log(finalAns)
+
+// let ans = op1 + op2;
+// console.log(ans);
+
+// console.log(op1.toUpperCase());
+// console.log(op2.toLowerCase());
+
+// let str = "Abhinav";
+// console.log(str.substring(2,4))
+// console.log(str.substring(2));
+
+
+// let sentence = "Hello Jee \"Kaise\" ho saare";
+// let sentence = "Hello Jee "Kaise" ho saare";
+// console.log(sentence)
+
+// let words = sentence.split(' ');
+
+// console.log(words);
+
+
+
+
+
